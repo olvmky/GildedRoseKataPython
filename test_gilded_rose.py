4016f5d673efb7bb1de13d5fb94879c1cd79b92c
olvmky <97950702+olvmky@users.noreply.github.com>
2024-10-01 22:42:11 -0700

    lab4

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -10,7 +10,25 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
+    
+    def test_conjured_items_degrade_twice_as_fast(self):
+        items = [Item("Conjured Mana Cake", 3, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(4, items[0].quality)  
 
+    def test_quality_never_negative_after_multiple_updates(self):
+        items = [Item("+5 Dexterity Vest", 1, 1)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        gilded_rose.update_quality()
+        self.assertEqual(1, items[0].quality)
+    
+    def test_aged_brie_increases_quality_after_sellin_passed(self):
+        items = [Item("Aged Brie", 0, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(13, items[0].quality)  
 
 if __name__ == '__main__':
     unittest.main()
